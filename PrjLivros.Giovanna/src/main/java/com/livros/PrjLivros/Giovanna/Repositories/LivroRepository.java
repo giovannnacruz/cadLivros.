bf8399c1fb6efb08d4fdcb7332c5ed8a5964c0e5
giovanna cristina <133894598+giovannnacruz@users.noreply.github.com>
2023-11-06 15:53:23 -0300

    Delete PrjLivros.Giovanna directory

--- PrjLivros.Giovanna/src/main/java/com/livros/PrjLivros/Giovanna/Repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.livros.PrjLivros.Giovanna.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.livros.PrjLivros.Giovanna.Entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-
-}
